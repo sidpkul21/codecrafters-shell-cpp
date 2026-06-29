de6cd304006b7d778e3c6ecbbf6a42c297fa1472
siddhesh <sidpkul21@gmail.com>
2026-06-28 22:17:33 -0400

    codecrafters submit [skip ci]

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -126,7 +126,7 @@ Command Parser::parse(const std::string& input) const{
     command.redirect_target = "";
 
     for(size_t i = 1; i < tokens.size(); i ++) {
-        if(tokens[i] == ">" || tokens[i] == "1>") {
+        if(tokens[i] == ">") {
             if((i + 1) < tokens.size()) {
                 i += 1;
                 command.redirect_target = tokens[i];
